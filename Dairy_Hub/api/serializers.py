fb0ae3b56236f29365181b38bec9fbf1fe4b5360
mutaiedithkandie <mutaiedithkandie@gmail.com>
2025-06-26 16:17:40 +0300

    Codes to the Orders API

---
+++ Dairy_Hub/api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from Orders.models import Order
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Order
+        fields="__all__"
+
+    
