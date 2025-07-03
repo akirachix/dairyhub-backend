d62a5e33852a8ab0f907495dedff9993b9065088
mutaiedithkandie <mutaiedithkandie@gmail.com>
2025-07-03 16:05:49 +0300

    added the order api

--- Dairy_Hub/Orders/models.py
+++ Dairy_Hub/Orders/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-# from farmers.models import Farmer
+from farmers.models import Farmer
 # Create your models here.
 class Order(models.Model):
     id = models.AutoField(primary_key=True)
-    # farmer_id=models.ForeignKey(Farmer,on_delete=models.CASCADE)
+    farmer_id=models.ForeignKey(Farmer,on_delete=models.CASCADE)
     order_date = models.DateField()
     status=models.TextField()
     price = models.DecimalField(decimal_places = 2,max_digits = 6)
     def __str__(self):
       return {self.name.id}
-
 
--- Dairy_Hub/api/tests.py
+++ Dairy_Hub/api/tests.py
@@ -1,3 +1,58 @@
+
 from django.test import TestCase
 
-# Create your tests here.
+Create your tests here.
+
+from rest_framework.test import APITestCase
+
+from rest_framework import status
+
+from django.urls import reverse
+
+from Orders.models import Order
+
+
+class OrderTests(APITestCase):
+    def setUp(self):
+        self.farmer = farmer.objects.create(full_name="Mwkali", email="mukali@gmail.com", phone="0724973222")
+        self.Order_data = {
+            "status": "Pending",
+            "price": "1000.00",
+            "order_date":"2025-06-11",
+            "farmer_id": self.farmer.id,
+        }
+
+    def test_post_Order(self):
+        url = reverse('Orders-list')
+        response = self.client.post(url, self.Order_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_Order(self):
+        Order.objects.create(**self.Order_data)
+        url = reverse('Orders-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_put_Order(self):
+        Order.objects.create(**self.Order_data)
+        order = Order.objects.first()
+        url = reverse('Orders-detail', args=[order.id])
+        response = self.client.put(url, self.Order_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+     
+    def test_delete_Order(self):
+        Order.objects.create(**self.Order_data)
+        order = Order.objects.first()
+        url = reverse('Orders-detail', args=[order.id])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+    
+
+
+    
+    
+
+
+
+
