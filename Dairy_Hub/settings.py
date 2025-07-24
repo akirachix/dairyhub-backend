0bc8467d1b13173a07e05d19561d7b01e78b93c2
Linn Amondi <linnamondi@gmail.com>
2025-07-24 10:05:50 +0300

    added cors headers

--- Dairy_Hub/settings.py
+++ Dairy_Hub/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
     'Orders',
     'rest_framework',
     'api',
